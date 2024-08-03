4b19e145d7c1fe529a648c4370dda4a4d24baeba
Анна Кочерыгина <annvikky@gmail.com>
2024-08-03 16:24:51 +0800

    add funcs transaction_descriptions(), card_number_generator() and tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,7 +2,11 @@ from typing import Dict, List
 
 import pytest
 
-from src.generators import filter_by_currency
+from src.generators import (
+    card_number_generator,
+    filter_by_currency,
+    transaction_descriptions,
+)
 
 transactions = [
     {
@@ -132,3 +136,66 @@ def test_filter_by_currency_stop_iter() -> None:
         output = filter_by_currency(transactions, "USD")
         for _ in range(2):
             next(output)
+
+
+def test_filter_by_currency_with_raise() -> None:
+    # тест на выбрасывание ошибки
+    with pytest.raises(Exception):
+        next(filter_by_currency(None, "USD"))
+
+
+@pytest.mark.parametrize(
+    "list_of_transactions, expected_descriptions",
+    [
+        (
+            transactions,
+            [
+                "Перевод организации",
+                "Перевод со счета на счет",
+                "Перевод со счета на счет",
+                "Перевод с карты на карту",
+                "Перевод организации",
+            ],
+        ),
+        ([], []),
+        ([{"id": 939719570}], [None]),
+    ],
+)
+def test_transaction_descriptions(
+    list_of_transactions: List[Dict[str, Dict]], expected_descriptions: List[str]
+) -> None:
+    # тест на корректность получения описаний по очереди
+    descriptions = transaction_descriptions(list_of_transactions)
+    assert list(descriptions) == expected_descriptions
+
+
+@pytest.mark.parametrize(
+    "num_1, num_2, expected_card_number",
+    [
+        (1, 1, ["0000 0000 0000 0001"]),
+        (
+            1,
+            5,
+            [
+                "0000 0000 0000 0001",
+                "0000 0000 0000 0002",
+                "0000 0000 0000 0003",
+                "0000 0000 0000 0004",
+                "0000 0000 0000 0005",
+            ],
+        ),
+        (9999_9999_9999_9999, 9999_9999_9999_9999, ["9999 9999 9999 9999"]),
+    ],
+)
+def test_card_number_generator(
+    num_1: int, num_2: int, expected_card_number: str
+) -> None:
+    # тест на корректность получения номера карты
+    assert list(card_number_generator(num_1, num_2)) == expected_card_number
+
+
+@pytest.mark.parametrize("num_1, num_2", [(1, 0), (-1, 5), ("1", 3)])
+def test_card_number_generator_with_raise(num_1: int, num_2: int) -> None:
+    # тест на выбрасывание ошибок
+    with pytest.raises(ValueError):
+        list(card_number_generator(num_1, num_2))
