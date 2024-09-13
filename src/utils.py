c1873c51318327d1793703af950e8c04394532c0
Анна Кочерыгина <annvikky@gmail.com>
2024-09-13 08:25:33 +0800

    add func get_transactions_list

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+import os.path
+
+
+PATH = os.path.join(os.path.dirname(__file__), "..", 'data', 'operations.json')
+
+def get_transactions_list(path: str) -> list[dict]:
+    """ Получение списка с финансовыми транзакциями"""
+    try:
+        with open(PATH, "r", encoding="utf-8") as file:
+            try:
+                transactions_list = json.load(file)
+                return transactions_list
+            except json.JSONDecodeError:
+                transactions_list = []
+                return transactions_list
+    except FileNotFoundError:
+        transactions_list = []
+        return transactions_list
+
+
+print(get_transactions_list(PATH))
---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+# import os
+#
+# import pytest
+#
+# PATH = os.path.join(os.path.dirname(__file__), "..", 'data', 'operations.json')
+#
+# @pytest.fixture
+#
+# def get_transaction_list(PATH):
